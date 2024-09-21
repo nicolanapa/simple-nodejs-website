f1b55a47d244096e0ff4907988686bf89d9d5da5
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-09-21 18:35:43 +0200

    Fixed Error ENOENT by adding __filename and __dirname, added routing of 404.html and index.html

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
 
-function return200(req, res) {
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const return200 = (req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-}
+};
 
-function return404(req, res) {
+const return404 = (req, res) => {
     res.writeHead(404, { "Content-Type": "text/html" });
-}
+};
 
-const server = http.createServer((req, res) => {
+const page404 = async (req, res) => {
+    try {
+        let errorPage = await fs.readFile(__dirname + "/project/404.html");
+        res.write(errorPage);
+    } catch (error) {
+        console.log("Couldn't load webpage:", error);
+    }
+};
+
+const server = http.createServer(async (req, res) => {
     console.log(req.method, req.url);
 
-    if (req.method === "GET") {
-    } else {
-        return404(req, res);
-        // 404.html
+    try {
+        if (req.method === "GET") {
+            if (req.url === "/") {
+                return200(req, res);
+
+                let file = await fs.readFile(__dirname + "/project/index.html");
+                res.write(file);
+            } else {
+                return404(req, res);
+                page404(req, res);
+            }
+        } else {
+            return404(req, res);
+            page404(req, res);
+        }
+    } catch (error) {
+        console.log("Couldn't load webpage:", error);
     }
 
     res.end();
