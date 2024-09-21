107ccad77a274fa14b80490c4679706e2edd6ee0
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-09-21 20:51:14 +0200

    Added checking for css requests and improved the return200 and return404 with a type parameter to remove double functions

--- src/index.js
+++ src/index.js
@@ -6,21 +6,18 @@ import path from "path";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const return200 = (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+const return200 = (req, res, type = "text/html") => {
+    res.writeHead(200, { "Content-Type": type });
 };
 
-const return404 = (req, res) => {
-    res.writeHead(404, { "Content-Type": "text/html" });
+const return404 = (req, res, type = "text/html") => {
+    res.writeHead(404, { "Content-Type": type });
 };
 
 const page404 = async (req, res) => {
-    try {
-        let errorPage = await fs.readFile(__dirname + "/project/404.html");
-        res.write(errorPage);
-    } catch (error) {
-        console.log("Couldn't load webpage:", error);
-    }
+    let errorPage = await fs.readFile(__dirname + "/project/404.html");
+    res.write(errorPage);
+    res.end();
 };
 
 const normalProjectReturner = async (req, res) => {
@@ -29,16 +26,33 @@ const normalProjectReturner = async (req, res) => {
     //console.log(finalPath);
     let file = await fs.readFile(__dirname + "/project" + finalPath);
     res.write(file);
+    res.end();
+};
+
+const otherResourceReturner = async (req, res) => {
+    console.log(__dirname + req.url);
+    let file = await fs.readFile(__dirname + req.url);
+    res.write(file);
+    res.end();
 };
 
 const server = http.createServer(async (req, res) => {
-    console.log(req.method, req.url);
+    console.log(req.method, req.url, req.headers.accept);
 
     try {
         if (req.method === "GET") {
             if (req.url === "/" || req.url === "/about" || req.url === "/contact-me") {
                 return200(req, res);
                 await normalProjectReturner(req, res);
+            } else if (req.url.substring(0, 8) === "/styles/") {
+                if (req.url === "/styles/404.css") {
+                    return200(req, res, "text/css");
+                    otherResourceReturner(req, res);
+                } else {
+                    return404(req, res, "text/css");
+                    res.write("");
+                    res.end();
+                }
             } else {
                 return404(req, res);
                 await page404(req, res);
@@ -48,10 +62,8 @@ const server = http.createServer(async (req, res) => {
             await page404(req, res);
         }
     } catch (error) {
-        console.log("Couldn't load webpage:", req.url, "with ERROR", error);
+        throw new Error(error);
     }
-
-    res.end();
 });
 
 server.listen(8080);
